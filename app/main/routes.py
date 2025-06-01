f7046324b91b47343284c3567aefbb0a6bab1457
aruttle <alan.ruttle@analog.com>
2025-06-01 18:39:01 +0100

    Added db tables for accommodations and bookings

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,16 +1,35 @@
+# from flask import render_template
+# from app.main import main
+# from app.models import Guest, Accommodation, Booking 
+
+
+# @main.route('/')
+# def home():
+#     projects = Project.query.all()
+#     return render_template('home.html', projects=projects)
+# @main.route('/about')
+# def about():
+#     return render_template('about.html')
+# @main.route('/accommodations')
+# def accommodations():
+#     projects = Project.query.all()
+#     return render_template('accommodations.html', projects=projects)
 from flask import render_template
 from app.main import main
-from app.models import Project
+from app.models import Accommodation
+
 
 @main.route('/')
 def home():
-    projects = Project.query.all()
-    return render_template('home.html', projects=projects)
+    return render_template('home.html')
+
 @main.route('/about')
 def about():
     return render_template('about.html')
+
 @main.route('/accommodations')
 def accommodations():
-    projects = Project.query.all()
-    return render_template('accommodations.html', projects=projects)
+    accommodations = Accommodation.query.all()
+    return render_template('accommodations.html', accommodations=accommodations)
+
 
--- app/models.py
+++ app/models.py
@@ -1,9 +1,27 @@
-from app import db
+from app import db  # Import the single shared db instance
 
-class Project(db.Model):
+class Guest(db.Model):
+    __tablename__ = 'guests'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=True)
 
-    def __repr__(self):
-        return f'<Project {self.name}>'
+    bookings = db.relationship('Booking', backref='guest', lazy=True)
+
+class Accommodation(db.Model):
+    __tablename__ = 'accommodations'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    price_per_night = db.Column(db.Float, nullable=False)
+
+    bookings = db.relationship('Booking', backref='accommodation', lazy=True)
+
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    accommodation_id = db.Column(db.Integer, db.ForeignKey('accommodations.id'), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
