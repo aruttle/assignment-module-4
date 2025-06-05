aec2e78d067a3773d266d68f99e2a6f322721801
aruttle <alan.ruttle@analog.com>
2025-06-05 23:12:23 +0100

    Still trying to get Render to work with db

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
-from app import create_app
+
+from app import create_app, db
+from flask_migrate import upgrade
+from app.models import *  
 
 app = create_app()
+
+# Run database migrations at startup
+with app.app_context():
+    upgrade()
