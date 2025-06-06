d7c32090a4dc7b29b0bc0b2beab63ab5752f9ad3
aruttle <alan.ruttle@analog.com>
2025-06-06 12:39:52 +0100

    Removed migrations and updated init_db.py

--- app/__init__.py
+++ app/__init__.py
@@ -24,8 +24,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    migrate.init_app(app, db)
-
+    
     # Register blueprint
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
